733262487a84507d3eea53f0577f704e20af31d6
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-04-14 19:28:09 +0530

    some sample examples

--- index.ts
+++ index.ts
@@ -33,3 +33,77 @@ const obj: ObjType2 = {
     working: true,
     id: 1
 }
+
+
+
+// Problem 1
+const func = (a:number,b:number) => {
+    return a*b ;
+};
+
+
+// Problem 2
+const func1 = (a:number, b:number) => {
+    return a/b;
+};
+
+
+// Problem 3
+let tuple: [string, boolean] = ["hello", true];
+
+
+// Problem 4
+interface myobj {
+    title : string,
+    status: boolean,
+    id: number
+  }
+  let Obj : myobj = {
+    title: 'anand',
+    status: true,
+    id: 1
+  };
+
+
+
+// Problem 5
+  const getName = (firstname:string, lastname?:string): string =>{
+    if(lastname){
+        const fullname = `${firstname} ${lastname}`;
+        return fullname;
+    }else{
+        return firstname;
+    }
+}
+
+
+
+
+// Problem 6 
+
+// `Fiction`, `Documentary` and `Educational`
+
+type Fiction = 'Fiction';
+type Documentary = 'Documentary';
+type Educational = 'Educational';
+export class Book {
+    constructor(public section :Fiction | Documentary | Educational,public name :string){}
+}
+
+export class Section{
+    public books : Book[] = [];
+    constructor(public name :Fiction | Documentary | Educational){}
+
+    addBook(book:Book){
+        this.books.push(book);
+    }
+}
+
+export class Library{
+    public sections : Section[] = [];
+    constructor(public name :string){}
+
+    addSection(section:Section){
+        this.sections.push(section);
+    }
+}
