4e2a2f0f69d226d4da414284e73581d5950fd5f2
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-04-16 19:15:09 +0530

    parking slot

--- LLD_Parking_Slot/index.ts
+++ LLD_Parking_Slot/index.ts
@@ -1,81 +1,53 @@
+type VehicleType = "Car" | "Bike" | "Bus";
+
+
 export class Vehicle {
-    VehicleType: 'Bus' | 'Car' | 'Bike';
-    registrationNumber: number;
-    color: string;
-
-    constructor(
-        VehicleType: "Bus" | "Car" | "Bike",
-        registrationNumber: number,
-        color: string
-    ) {
-        this.VehicleType = VehicleType;
-        this.registrationNumber = registrationNumber;
-        this.color = color;
-    }
+    constructor(public vehicleType: VehicleType, public registrationNumber: number, public color: string) { }
 }
-
 
 export class Bike extends Vehicle {
-    constructor(registrationNumber: number, color: string) {
-        super('Bike', registrationNumber, color)
+    constructor(public registrationNumber: number, public color: string) {
+        super("Bike", registrationNumber, color)
     }
 }
-
 export class Bus extends Vehicle {
-    constructor(registrationNumber: number, color: string) {
-        super('Bus', registrationNumber, color)
+    constructor(public registrationNumber: number, public color: string) {
+        super("Bus", registrationNumber, color)
     }
 }
-
 export class Car extends Vehicle {
-    constructor(registrationNumber: number, color: string) {
-        super('Car', registrationNumber, color)
+    constructor(public registrationNumber: number, public color: string) {
+        super("Car", registrationNumber, color)
     }
 }
 
 export class Slot {
-    type: 'Car' | 'Bus' | 'Bike';
-    isBooked: boolean;
-    constructor(type: 'Car' | 'Bus' | 'Bike', isBooked: boolean) {
-        this.type = type;
-        this.isBooked = isBooked;
-    }
+    public isBooked: boolean = false;
+    constructor(public type: VehicleType) { }
 }
 
 export class ParkingSlot {
-    maxLimit: number;
-    parkingSpots: Slot[];
-
-    constructor(maxLimit: number) {
-        this.maxLimit = maxLimit;
+    public parkingSpots: Slot[];
+    constructor(public maxLimit: number) {
         this.parkingSpots = [];
     }
 
-    addSlots(slot: Slot) {
+    addSlots(slot: Slot): number {
         if (this.parkingSpots.length < this.maxLimit) {
-            this.parkingSpots.push(slot)
+            this.parkingSpots.push(slot);
         }
         return this.parkingSpots.length;
     }
 
-    availableSlots(VehicleType: 'Bus' | 'Car' | 'Bike') {
-        let count = 0;
-        for (let i = 0; i < this.parkingSpots.length; i++) {
-            if (this.parkingSpots[i].type == VehicleType &&
-                this.parkingSpots[i].isBooked == false) {
-                count++;
-            }
-        }
-        return count;
+    availableSlot(vehicleType: VehicleType): number {
+        return this.parkingSpots.filter((slot) => slot.type === vehicleType && !slot.isBooked).length;
     }
 
-    bookSlot(vehicle: Vehicle) {
-        for (let i = 0; i < this.parkingSpots.length; i++) {
-            if (this.parkingSpots[i].type == vehicle.VehicleType &&
-                this.parkingSpots[i].isBooked == false) {
-                this.parkingSpots[i].isBooked = true;
-                return true;
-            }
+    bookSlot(vehicle: Car | Bike | Bus): boolean {
+        const slotNumber = this.parkingSpots.findIndex((slot) => slot.type === vehicle.vehicleType && !slot.isBooked);
+        if (slotNumber !== -1) {
+            this.parkingSpots[slotNumber].isBooked = true;
+            return true;
         }
         return false;
     }
